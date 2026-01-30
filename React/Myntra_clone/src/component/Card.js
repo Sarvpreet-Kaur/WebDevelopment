fb14c392566aaaa15995ccb464e2a1897a81a9ed
Sarvpreet-Kaur <sarvpreetk22@gmail.com>
2026-01-30 14:58:45 +0530

    Hooks

--- React/Myntra_clone/src/component/Card.js
+++ React/Myntra_clone/src/component/Card.js
@@ -1,14 +1,12 @@
-function Card(props) {
+export default function Card(props){
     return (
-        <div className="card" style={{ border: "2px solid black", padding: "2px" }}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsfsuPY-UEkBkdshjJqVmUi-50fBF92j31tA&s" height="200px" width="200px" />
-            <div style={{ textAlign: "center" }}>
+        <div className="card" style = {{border: "2px solid black", padding:"2px"}}>
+            <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVAzA7ex7j4uWmHS7bvJGy9R88mJZepvNiiU7JMfVStg&s" height="200px" width="150px"/>
+            <div style = {{textAlign:"center"}}> 
                 <h2>{props.cloth}</h2>
                 <h1>{props.offer}</h1>
-                <h2>Shop Now</h2>
+                <h2>Price: {props.price}</h2>
             </div>
         </div>
-    );
+    )
 }
-
-export default Card
--- React/Myntra_clone/src/myntra.js
+++ React/Myntra_clone/src/myntra.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Card from "./component/Card"
 import Header from "./component/header";
 import arr from "./utils/dummy"
 
+console.log(arr);
+
 function App() {
+  let [A, setA] =useState(arr);
+
+  function sortArray(){
+    const sorted = [...A].sort((a,b)=>{ return a.price - b.price})
+    console.log(sorted);
+    setA(sorted)
+  }
+
+  function Price_above_9999(){
+    const above_9999 = arr.filter(value => value.price>9999)
+    setA(above_9999)
+  }
     return (
         <>
           <div>
             <Header/>
+            <button onClick = {sortArray}>Sortby Price</button>
+            <button onClick = {Price_above_9999}>Price above 9999</button>
           </div>
           <div className="bod" style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
               {/* <Card cloth = "T-Shirt" offer = "20-30%"/>
@@ -20,7 +36,7 @@ function App() {
               <Card cloth = "T-Shirt" offer = "20-30%"/> */}
 
               {
-                arr.map((value, index)=> <Card key = {index} cloth = {value.cloth} offer = {value.offer}/>)
+                A.map((value, index)=> <Card key = {index} cloth = {value.cloth} offer = {value.offer} price = {value.price}/>)
               }
       
           </div>
