260fbc0edfd3a6cdfa65e8c95c3f81f774d34256
Sarvpreet-Kaur <sarvpreetk22@gmail.com>
2026-02-27 23:53:23 +0530

    TailwindCSS

---
+++ TailwindCSS/src/app.js
@@ -0,0 +1,303 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import "./index.css";
+import { your } from './../../React/useMemohook/dist/useMemohook.df1a86d5';
+
+function App(){
+    return(
+        <div className=" flex flex-wrap justify-center gap-10">
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://raketcontent.com/large_Pink_Watercolor_Quotes_Desktop_Wallpaper_df5664cfda.png"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Keep Going with your Bad SELF !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw5d98tFPNNdFbn36pRO6zH9fE3gtpvTj2sQ&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Give your BEST in EVERYTHING !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-877zzKeDfY3dx-qTsTzg0iXz9CFOnYCWXg&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>MEANT FOR YOUUU !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXaG8SR8K5U0ovjqlTFL9CkNWA_BoMjpQa2Q&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>A DEEP BREATH !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyzJ-igJGSzR_XthO0yhvTPyKqPfKpJMpvJQ&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Keep Going with your Bad SELF !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://marketplace.canva.com/EAGXqrB6AbA/2/0/1600w/canva-dark-blue-and-cream-modern-international-mountain-day-quote-desktop-wallpaper-Dh_Y5ZciLm0.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>IT IS FOR YoU ONLY !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://www.hdwallpapers.in/download/good_things_take_time_hd_motivational-1600x900.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Keep Going !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJRI9ysDZbgGw4CIRWXciAmGauWP12p8D4OQ&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>SMILE you are WORTH IT !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://raketcontent.com/large_Pink_Watercolor_Quotes_Desktop_Wallpaper_df5664cfda.png"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Keep Going with your Bad SELF !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw5d98tFPNNdFbn36pRO6zH9fE3gtpvTj2sQ&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Give your BEST in EVERYTHING !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-877zzKeDfY3dx-qTsTzg0iXz9CFOnYCWXg&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>MEANT FOR YOUUU !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXaG8SR8K5U0ovjqlTFL9CkNWA_BoMjpQa2Q&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>A DEEP BREATH !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyzJ-igJGSzR_XthO0yhvTPyKqPfKpJMpvJQ&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Keep Going with your Bad SELF !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://marketplace.canva.com/EAGXqrB6AbA/2/0/1600w/canva-dark-blue-and-cream-modern-international-mountain-day-quote-desktop-wallpaper-Dh_Y5ZciLm0.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>IT IS FOR YoU ONLY !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://www.hdwallpapers.in/download/good_things_take_time_hd_motivational-1600x900.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Keep Going !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJRI9ysDZbgGw4CIRWXciAmGauWP12p8D4OQ&s"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Todays's Motivation</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>SMILE you are WORTH IT !!!!!!</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-pink-400 p-1 rounded-xl text-white text-2xl hover:bg-pink-500">Keep it GIRLIE!!</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App></App>)
